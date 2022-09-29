b085b9a485cb8795617aa3ec007b114de0fd2c84
SheikhMujib <mbasheikhmujib@gmail.com>
2022-09-29 19:39:34 +0600

    Activity component added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Activity from '../Activity/Activity';
 
 const Main = () => {
     const [activities, setActivities] = useState([]);
@@ -6,21 +7,24 @@ const Main = () => {
     useEffect(() => {
         fetch('activities.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setActivities(data))
     }, [])
     return (
         <div class="container">
             <div class="row">
                 {/* Activities container */}
-                <div class="col-sm-9 bg-light">
-                    <div class="row">
-                        <div class="col-sm">col-sm</div>
-                        <div class="col-sm">col-sm</div>
-                        <div class="col-sm">col-sm</div>
+                <div className='col-sm-9 bg-light mt-5'>
+                    <h1>Pro Web Developer</h1>
+                    <h5 className='my-5'>Select Today's Practice Plan</h5>
+                    <div className='row row-cols-1 row-cols-sm-3 g-4'>
+                        {
+                            activities.map(activity => <Activity></Activity>)
+                        }
                     </div>
+
                 </div>
                 {/* Cart container */}
-                <div class="col-sm-3 bg-warning">col-sm-3</div>
+                <div className='col-sm-3 bg-warning'>col-sm-3</div>
             </div>
         </div>
     );
