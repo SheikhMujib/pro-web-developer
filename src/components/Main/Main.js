27178871f69adedc70ad5709d64237aa36896e57
SheikhMujib <mbasheikhmujib@gmail.com>
2022-09-29 18:49:23 +0600

    Fake data file added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Main = () => {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        fetch('activities.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
         <div class="container">
             <div class="row">
                 {/* Activities container */}
-                <div class="col-sm-9 bg-primary">
+                <div class="col-sm-9 bg-light">
                     <div class="row">
-                        <div class="col-sm-4">col-sm</div>
-                        <div class="col-sm-4">col-sm</div>
-                        <div class="col-sm-4">col-sm</div>
+                        <div class="col-sm">col-sm</div>
+                        <div class="col-sm">col-sm</div>
+                        <div class="col-sm">col-sm</div>
                     </div>
                 </div>
                 {/* Cart container */}
